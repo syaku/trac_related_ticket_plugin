f1750b925f97da8b2611e143cc754b50cf4eaf6d
Takayuki TAKEMURA <syaku@sevenspirals.net>
2011-07-09 23:30:31 +0900

    setup.pyのパッケージ情報を更新. refs #55

--- RelatedTicketPlugin/src/setup.py
+++ RelatedTicketPlugin/src/setup.py
@@ -6,6 +6,8 @@ VERSION = '0.1'
 
 setup(name=PACKAGE,
       version=VERSION,
+      author='@syaku',
+      author_email='syaku@sevenspirals.net',
       packages=['related_ticket'],
       entry_points={'trac.plugins': '%s = related_ticket' % PACKAGE},
 )
